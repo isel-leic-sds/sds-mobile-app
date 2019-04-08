68ffb278f143910bce19949e7b8441c87ac3f5e1
MiguelNogueira <a39202@alunos.isel.pt>
2019-04-08 01:08:47 +0100

    MDS-004: Add support to profile activity
    (cherry picked from commit 0ef685996decb2c5f741e56b4779cbd29feae5c0)

---
+++ app/src/main/java/com/isel/ps/sds/DhsRepository.kt
@@ -0,0 +1,33 @@
+package com.isel.ps.sds
+
+import android.content.Context
+import com.isel.ps.sds.view.profile.Person
+import com.isel.ps.sds.view.profile.SosContact
+import org.json.JSONObject
+import java.io.InputStream
+import java.sql.Date
+
+class DhsRepository {
+
+    fun loadPersonData(context: Context, onSuccess: (Person) -> Unit, onError: (String?) -> Unit) {
+        try {
+            val inputStream: InputStream = context.assets.open("personData.json")
+            val inputString = inputStream.bufferedReader().use { it.readText() }
+            val obj = JSONObject(inputString)
+            val sosContact = obj.getJSONObject("sosContact")
+            val person = Person(
+                obj.getString("name"),
+                Date.valueOf(obj.getString("dateOfBirth")),
+                obj.getInt("nif"),
+                obj.getInt("phoneNumber"),
+                SosContact(
+                    sosContact.getString("name"),
+                    sosContact.getInt("phoneNumber")
+                )
+            )
+            onSuccess(person)
+        } catch (e: Exception) {
+            onError(e.message)
+        }
+    }
+}
