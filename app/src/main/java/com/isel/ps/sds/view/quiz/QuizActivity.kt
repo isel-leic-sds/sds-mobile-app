0af9463c8e7fe09df829c46b2d1a047251619b52
Gman9 <gmanunes9@gmail.com>
2019-05-10 22:53:51 +0100

    Implementação do primeiro fragmento
    Alguns layouts são para apagar mais tarde

--- app/src/main/java/com/isel/ps/sds/view/quiz/QuizActivity.kt
+++ app/src/main/java/com/isel/ps/sds/view/quiz/QuizActivity.kt
@@ -1,13 +1,20 @@
 package com.isel.ps.sds.view.quiz
 
+import android.net.Uri
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import com.isel.ps.sds.R
 import com.isel.ps.sds.view.BaseActivity
+import com.isel.ps.sds.view.quiz.fragments.RadioAnsFragment
+import kotlinx.android.synthetic.main.fragment_radio_ans.*
 
 
+class QuizActivity : BaseActivity<QuizViewModel>(),RadioAnsFragment.OnFragmentInteractionListener {
+    override fun onFragmentInteraction(uri: Uri) {
+        var radio = radioButton.text
+    }
 
-class QuizActivity : BaseActivity<QuizViewModel>() {
     override fun defineViewModel(): Class<QuizViewModel> = QuizViewModel::class.java
     override fun layoutToInflate(): Int = R.layout.activity_loading
 
@@ -18,6 +25,18 @@ class QuizActivity : BaseActivity<QuizViewModel>() {
 
         viewModel.getQuiz()?.observe(this, Observer { quiz ->
 
+            var quest = Question("id1","questãoooooo",Answer("coco","xixi"))
+            supportFragmentManager.beginTransaction().replace(R.id.frame_question,RadioAnsFragment()).commit()
+           /*
+            var transaction = mang.beginTransaction()
+
+            // Replace the fragment on container
+            transaction.
+            transaction.addToBackStack(null)
+
+            // Finishing the transition
+            transaction.commit()
+            */
 /*
             var questsList = quiz.listQuestions
             var quest = questsList.first
@@ -27,7 +46,7 @@ class QuizActivity : BaseActivity<QuizViewModel>() {
             var ans = quest.possibleAnswers
             radioButton.text=ans.option1
             radioButton2.text=ans.option2
-            setContentView(R.layout.activity_radio_answer)
+            setContentView(R.layout.fragment_radio_answer)
 */
         })
 
