94dc53b057e076189aa2c0897774469855b31fe3
Gman9 <gmanunes9@gmail.com>
2019-09-03 17:14:53 +0100

    HistoryScheduleFragment is done!

--- app/src/main/java/com/isel/ps/sds/view/clinicalHistory/fragments/HistoryScheduleFragment.kt
+++ app/src/main/java/com/isel/ps/sds/view/clinicalHistory/fragments/HistoryScheduleFragment.kt
@@ -1,7 +1,9 @@
 package com.isel.ps.sds.view.clinicalHistory.fragments
 
+import android.os.Build
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.text.format.DateFormat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,7 +18,12 @@ import com.github.mikephil.charting.utils.ColorTemplate
 import com.isel.ps.sds.R
 import com.isel.ps.sds.view.clinicalHistory.data.ClinicalHistoryData
 import kotlinx.android.synthetic.main.history_schedule_fragment.*
+import java.sql.Date
+import java.sql.Time
 import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class HistoryScheduleFragment(var clinicalHistory: ClinicalHistoryData) : Fragment() {
 
@@ -40,12 +47,19 @@ class HistoryScheduleFragment(var clinicalHistory: ClinicalHistoryData) : Fragme
 
        var  barEntries = ArrayList<BarEntry> ()
 
-        barEntries.add(BarEntry(0F, 5F))
-        barEntries.add(BarEntry(1F, 10F))
-        barEntries.add(BarEntry(2F, 15F))
-       barEntries.add(BarEntry(3F, 1F))
+        var faseDay = ArrayList<Int>(4)
+        faseDay.add(0,0)
+        faseDay.add(1,0)
+        faseDay.add(2,0)
+        faseDay.add(3,0)
 
-        var barDataSet = BarDataSet(barEntries,"PERGUNTA")
+        funcao(faseDay)
+        barEntries.add(BarEntry(0F, faseDay[0].toFloat()))
+        barEntries.add(BarEntry(1F, faseDay[1].toFloat()))
+        barEntries.add(BarEntry(2F, faseDay[2].toFloat()))
+        barEntries.add(BarEntry(3F, faseDay[3].toFloat()))
+
+        var barDataSet = BarDataSet(barEntries,clinicalHistory.question)
 
         barDataSet.colors = ColorTemplate.COLORFUL_COLORS.asList()
 
@@ -60,6 +74,8 @@ class HistoryScheduleFragment(var clinicalHistory: ClinicalHistoryData) : Fragme
         dayFases.add("(20h->23:59h")
 
 
+
+
         var xAxis=barCHart.xAxis
         xAxis.valueFormatter=MyAxisValueFormatter(dayFases)
 //        xAxis.position= XAxis.XAxisPosition.BOTH_SIDED
@@ -67,12 +83,45 @@ class HistoryScheduleFragment(var clinicalHistory: ClinicalHistoryData) : Fragme
  //      xAxis.setCenterAxisLabels(true)
 //        xAxis.axisMaximum=1F
 
+
     }
 
     class MyAxisValueFormatter(val dayFases: ArrayList<String>) : ValueFormatter() {
         override fun getFormattedValue(value: Float): String {
             return dayFases[value.toInt()]
         }
+    }
+
+
+    fun funcao(dayFases: ArrayList<Int>){
+        var answers = clinicalHistory.answers
+        var answer = ""
+        var size = 0
+        val zeroHours = SimpleDateFormat("HH:mm").parse("00:00")
+        val sevenHours = SimpleDateFormat("HH:mm").parse("07:00")
+        val twelveHours = SimpleDateFormat("HH:mm").parse("12:00")
+        val twentyHours = SimpleDateFormat("HH:mm").parse("20:00")
+        val twentyFourHours = SimpleDateFormat("HH:mm").parse("23:59")
+        for (i: Int in 0 until answers.size){
+           answer = answers[i].answer
+            size = answers[i].userAnswer.size
+           var date = SimpleDateFormat("HH:mm").parse(answer)
+            if(date>=zeroHours && date < sevenHours){
+                dayFases[0] = dayFases[0] + size
+            }
+            if(date>=sevenHours && date < twelveHours){
+                dayFases[1] = dayFases[1] + size
+            }
+            if(date>=twelveHours && date < twentyHours){
+                dayFases[2] = dayFases[2] + size
+            }
+
+            if(date>=twentyHours && date < twentyFourHours){
+                dayFases[3] = dayFases[3] + size
+            }
+
+        }
+        dayFases
     }
 
 
